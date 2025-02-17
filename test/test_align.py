ed3cb3b6c7f3c3ae7af37d566fe8bf6a6070a2c4
ipsamitt <ipsam001@gmail.com>
2025-02-16 19:23:14 -0800

    edit tests

--- test/test_align.py
+++ test/test_align.py
@@ -25,9 +25,17 @@ def test_nw_alignment():
                           [-13, -7, -7, -19], 
                           [-14, -8, -8, -6]])
     assert np.allclose(nw._gapA_matrix, true_gapA, atol=1e-6)
-    true_gapB = np.array([[-10, -11, -12, -13, -14], [-float('inf'), -float('inf'), -6, -7], [-float('inf'), -float('inf'), -23, -7], [-float('inf'), -float('inf'), -23, -12], [-float('inf'), -float('inf'), -25, -17]])
+    true_gapB = np.array([[-10, -11, -12, -13],
+                           [-float('inf'), -float('inf'), -6, -7], 
+                           [-float('inf'), -float('inf'), -23, -7],
+                             [-float('inf'), -float('inf'), -23, -12], 
+                             [-float('inf'), -float('inf'), -25, -17]])
     assert np.allclose(nw._gapB_matrix, true_gapB, atol=1e-6)
-    true_align = np.array([[0, -float('inf'), -float('inf'),-float('inf')], [-float('inf'), 5, -11, -13], [-float('inf'), -12, 4, -8], [-float('inf'), -12, -1, 5], [-float('inf'), -14, -6, 4]])
+    true_align = np.array([[0, -float('inf'), -float('inf'),-float('inf')],
+                            [-float('inf'), 5, -11, -13], 
+                            [-float('inf'), -12, 4, -8], 
+                            [-float('inf'), -12, -1, 5], 
+                            [-float('inf'), -14, -6, 4]])
     assert np.allclose(nw._align_matrix, true_align, atol=1e-6)
 
 def test_nw_backtrace():
